301e335e61d7911b457a5192481bf9dc1069acac
Kiyoko Ikeuchi <kiyoko.ikeuchi@gmail.com>
2016-07-26 13:57:33 -0700

    style: Modify and move mapping section

--- west-nile-virus/west_nile_virus_prediction.py
+++ west-nile-virus/west_nile_virus_prediction.py
@@ -1,7 +1,3 @@
-# This is an example of developing a script locally with the West Nile Virus data to share on Kaggle
-# Once you have a script you're ready to share, paste your code into a new script at:
-#	https://www.kaggle.com/c/predict-west-nile-virus/scripts/new
-
 # Code is borrowed from this script: https://www.kaggle.com/users/213536/vasco/predict-west-nile-virus/west-nile-heatmap
 
 import pandas as pd
@@ -19,27 +15,6 @@ weather = pd.read_csv('../input/weather.csv', na_values=['M', '-', ' '])
 display(train.head(), train.shape)
 display(spray.head(), spray.shape)
 display(weather.columns.values, weather.shape)
-
-# Initial visualization
-# Modified from Kaggle starter code
-mapdata = np.loadtxt("../input/mapdata_copyright_openstreetmap_contributors.txt")
-traps = pd.read_csv('../input/train.csv')[['Date', 'Trap','Longitude', 'Latitude', 'WnvPresent']]
-
-aspect = mapdata.shape[0] * 1.0 / mapdata.shape[1]
-lon_lat_box = (-88, -87.5, 41.6, 42.1)
-
-plt.figure(figsize=(10,14))
-plt.imshow(mapdata, 
-           cmap=plt.get_cmap('gray'), 
-           extent=lon_lat_box, 
-           aspect=aspect)
-
-#drop rows with off-the-map locations
-spray = spray[spray.Latitude < 42.2]
-spray_locations = spray[['Longitude', 'Latitude']].drop_duplicates().values
-locations = traps[['Longitude', 'Latitude']].drop_duplicates().values
-plt.scatter(spray_locations[:,0], spray_locations[:,1], marker='o', color = 'c')
-plt.scatter(locations[:,0], locations[:,1], marker='x')
 
 # Data Exploration
 # Exclude redundant columns from the training data
@@ -85,3 +60,30 @@ for col in weather_impute:
     median = weather[col].median()  
 # Data statistics after imputation
 display(weather[weather_impute].describe())
+
+# First 5 rows of the final weather data
+display(weather.head())
+
+# Visualize locations on the map
+# Modified from Kaggle starter code
+mapdata = np.loadtxt("../input/mapdata_copyright_openstreetmap_contributors.txt")
+traps = pd.read_csv('../input/train.csv')[['Date', 'Trap','Longitude', 'Latitude', 'WnvPresent']]
+
+aspect = mapdata.shape[0] * 1.0 / mapdata.shape[1]
+lon_lat_box = (-88, -87.5, 41.6, 42.1)
+
+plt.figure(figsize=(10,14))
+plt.imshow(mapdata, 
+           cmap=plt.get_cmap('gray'), 
+           extent=lon_lat_box, 
+           aspect=aspect)
+
+#drop rows with off-the-map locations
+spray = spray[spray.Latitude < 42.2]
+spray_locations = spray[['Longitude', 'Latitude']].drop_duplicates().values
+locations = traps[['Longitude', 'Latitude']].drop_duplicates().values
+stations = np.array([[-87.933, 41.995], [-87.752, 41.786, ]])
+plt.scatter(spray_locations[:,0], spray_locations[:,1], marker='x', color = 'y')
+plt.scatter(locations[:,0], locations[:,1], marker='o')
+for i in range(2):
+    plt.plot(stations[i,0], stations[i,1], '^', color='r')
